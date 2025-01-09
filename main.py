88dfa6981b882638341f0dcac3a6583d5d4e7b8e
Geetansh-Jangid <96619060+Geetansh-Jangid@users.noreply.github.com>
2025-01-09 21:31:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 import sys
-import unit_converter
 import chatbot
 import image_generator
-import graph_plotter
 import csv_editor
 import file_organiser
 import password_generator
 import file_organiser
-import weather
 
 def dashboard():
     print("\n=== Dashboard ===")
     print("1. Image Generator")
     print("2. Chatbot")
-    print("3. Unit Converter")
-    print("4. Graph Plotter")
-    print("5. CSV Editor")
-    print("6. File Organizer")
-    print("7. Password Generator")
-    print("8. Weather")
-    print("9. Exit")
+    print("3. CSV Editor")
+    print("4. File Organizer")
+    print("5. Password Generator")
+    print("6. Exit")
 
 def image_generator_o():
     image_generator.image_generator()
@@ -34,12 +28,6 @@ def chatbot_menu():
         response = chatbot.start_chatbot(user_input)
         print(f"Chatbot: {response}")
 
-def unit_converter_menu():
-    unit_converter.unit_converter()
-
-def graph_plotter_menu():
-    graph_plotter.graph_plotter()
-
 def csv_editor_menu():
     csv_editor.csv_editor()
 
@@ -48,9 +36,6 @@ def file_organizer_menu():
 
 def password_generator_menu():
     password_generator.password_generator()
-
-def weather_menu():
-    weather.weather()
 
 def main():
     while True:
@@ -62,19 +47,13 @@ def main():
         elif choice == '2':
             chatbot_menu()
         elif choice == '3':
-            unit_converter_menu()
-        elif choice == '4':
-            graph_plotter_menu()
-        elif choice == '5':
             csv_editor_menu()
             sys.exit()
-        elif choice == '7':
+        elif choice == '4':
+            file_organizer.file_organizer()
+        elif choice == '5':
             password_generator.password_generator()
         elif choice == '6':
-            file_organizer.file_organizer()
-        elif choice == '8':
-            weather.weather_checker()
-        elif choice == '9':
             print("Exiting program. Goodbye!")
             break
         else:
